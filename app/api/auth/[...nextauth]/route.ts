f4f77aa805fef336dc029dbcaafb032566c27e1b
choknt <153351426+choknt@users.noreply.github.com>
2025-09-30 06:28:26 +0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "./options";
+
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
+export { authOptions } from "./options";
